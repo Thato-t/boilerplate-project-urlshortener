d0f17af44f3126dfc08ff6493652bb1205eeb73a
Thato <albanostlabo0@gmail.com>
2025-02-27 22:12:42 +0200

    fix: made the short_url redirect to the original url

--- index.js
+++ index.js
@@ -7,17 +7,33 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.urlencoded({ extended: true}))
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+let originalURI;
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  originalURI = req.body.url
+  const regex = /^https:\/\/www.[a-z]+.org$/
+  
+  if(!regex.test(originalURI)){
+    res.json({ error: 'invalid url'})
+  } else{
+    res.json({ 
+      original_url: originalURI,
+      short_url: 1
+    });
+  }
 });
+
+app.get('/api/shorturl/:url?', (req, res) => {
+  const uri = originalURI;
+  res.redirect(uri)
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
